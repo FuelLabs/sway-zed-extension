63666b9ef6a7e750076cd5120395598aae16be54
Joshua Batty <joshpbatty@gmail.com>
2025-08-27 11:26:48 +1000

    Fix Sway extension configuration and syntax highlighting
    - Update tree-sitter grammar to latest commit (395006713)
    - Fix extension.toml format with proper language server configuration
    - Simplify language configuration to resolve TOML parsing errors
    - Add official tree-sitter-sway syntax highlighting queries
    - Fix forc-lsp invocation (no arguments needed)
    
    Extension now provides working syntax highlighting and LSP support.

--- languages/sway/highlights.scm
+++ languages/sway/highlights.scm
@@ -1,160 +1,335 @@
-; Identifiers
-(type_identifier) @type
+; -------
+; Tree-Sitter doesn't allow overrides in regards to captures,
+; though it is possible to affect the child node of a captured
+; node. Thus, the approach here is to flip the order so that
+; overrides are unnecessary.
+; -------
+
+; -------
+; Types
+; -------
+
+; ---
+; Primitives
+; ---
+
+(escape_sequence) @constant.character.escape
 (primitive_type) @type.builtin
-(field_identifier) @property
+(boolean_literal) @constant.builtin.boolean
+(integer_literal) @constant.numeric.integer
+(float_literal) @constant.numeric.float
+(char_literal) @constant.character
+[
+  (string_literal)
+  (raw_string_literal)
+] @string
+[
+  (line_comment)
+  (block_comment)
+] @comment
 
-; Identifier conventions
-; Assume all-caps names are constants
-((identifier) @constant
- (#match? @constant "^[A-Z][A-Z\d_]+$"))
+; ---
+; Extraneous
+; ---
 
-; Assume uppercase names are enum constructors
-((identifier) @constructor
- (#match? @constructor "^[A-Z]"))
+(self) @variable.builtin
+(enum_variant (identifier) @type.enum.variant)
 
-; Function calls
-(call_expression
- function: (identifier) @function)
+(field_initializer
+  (field_identifier) @variable.other.member)
+(shorthand_field_initializer
+  (identifier) @variable.other.member)
+(shorthand_field_identifier) @variable.other.member
 
-(macro_invocation
- macro: (identifier) @function.macro
- "!" @function.macro)
+(loop_label
+  "'" @label
+  (identifier) @label)
 
-; Function definitions
-(function_item (identifier) @function)
-
-; Method definitions
-(function_signature_item (identifier) @function)
-
-; Comments
-(line_comment) @comment
-(block_comment) @comment
-
-; Sway-specific keywords
-"abi" @keyword
-"as" @keyword
-"asm" @keyword
-"break" @keyword
-"configurable" @keyword
-"const" @keyword
-"continue" @keyword
-"contract" @keyword
-"deref" @keyword
-"else" @keyword
-"enum" @keyword
-"fn" @keyword
-"for" @keyword
-"if" @keyword
-"impl" @keyword
-"in" @keyword
-"let" @keyword
-"library" @keyword
-"match" @keyword
-"mod" @keyword
-"mut" @keyword
-"predicate" @keyword
-"pub" @keyword
-"ref" @keyword
-"return" @keyword
-"script" @keyword
-"Self" @keyword
-"self" @keyword
-"storage" @keyword
-"struct" @keyword
-"trait" @keyword
-"type" @keyword
-"use" @keyword
-"where" @keyword
-"while" @keyword
-
-; Storage keywords
-"read" @keyword
-"write" @keyword
-
-; Visibility modifiers
-"pub" @keyword
-
-; Literals
-(char_literal) @string
-(string_literal) @string
-(raw_string_literal) @string
-(boolean_literal) @constant.builtin
-(integer_literal) @number
-(float_literal) @number
-
-; Special constants
-"true" @constant.builtin
-"false" @constant.builtin
-
-; Operators
-"+" @operator
-"-" @operator
-"*" @operator
-"/" @operator
-"%" @operator
-"=" @operator
-"==" @operator
-"!=" @operator
-"<" @operator
-"<=" @operator
-">" @operator
-">=" @operator
-"&&" @operator
-"||" @operator
-"!" @operator
-"&" @operator
-"|" @operator
-"^" @operator
-"<<" @operator
-">>" @operator
-"+=" @operator
-"-=" @operator
-"*=" @operator
-"/=" @operator
-"%=" @operator
-"&=" @operator
-"|=" @operator
-"^=" @operator
-"<<=" @operator
-">>=" @operator
-"->" @operator
-"=>" @operator
-".." @operator
-"..=" @operator
-"::" @operator
-
+; ---
 ; Punctuation
-";" @punctuation.delimiter
-"," @punctuation.delimiter
-"." @punctuation.delimiter
-":" @punctuation.delimiter
+; ---
 
-; Brackets
-"(" @punctuation.bracket
-")" @punctuation.bracket
-"[" @punctuation.bracket
-"]" @punctuation.bracket
-"{" @punctuation.bracket
-"}" @punctuation.bracket
+[
+  "::"
+  "."
+  ";"
+  ","
+] @punctuation.delimiter
 
-; Attributes
-(attribute_item) @attribute
-(inner_attribute_item) @attribute
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+  "#"
+] @punctuation.bracket
+(type_arguments
+  [
+    "<"
+    ">"
+  ] @punctuation.bracket)
+(type_parameters
+  [
+    "<"
+    ">"
+  ] @punctuation.bracket)
+(closure_parameters
+  "|" @punctuation.bracket)
 
-; Macro rules
-(macro_definition (identifier) @function.macro)
+; ---
+; Variables
+; ---
 
-; Generic parameters
-(type_parameters (type_identifier) @type)
-(lifetime (identifier) @label)
+(let_declaration
+  pattern: [
+    ((identifier) @variable)
+    ((tuple_pattern
+      (identifier) @variable))
+  ])
+  
+; It needs to be anonymous to not conflict with `call_expression` further below. 
+(_
+ value: (field_expression
+  value: (identifier)? @variable
+  field: (field_identifier) @variable.other.member))
 
-; Labels
-(labeled_statement (label) @label)
-(break_expression (label) @label)
-(continue_expression (label) @label)
+(parameter
+	pattern: (identifier) @variable.parameter)
+(closure_parameters
+	(identifier) @variable.parameter)
 
-; Storage access
-(storage_access) @property
+; -------
+; Keywords
+; -------
 
-; Error propagation
-"?" @operator
+(for_expression
+  "for" @keyword.control.repeat)
+((identifier) @keyword.control
+  (#match? @keyword.control "^yield$"))
+
+"in" @keyword.control
+
+[
+  "match"
+  "if"
+  "else"
+] @keyword.control.conditional
+
+[
+  "while"
+] @keyword.control.repeat
+
+[
+  "break"
+  "continue"
+  "return"
+] @keyword.control.return
+
+[
+  "contract"
+  "script"
+  "predicate"
+] @keyword.other
+
+"use" @keyword.control.import
+(mod_item "mod" @keyword.control.import !body)
+(use_as_clause "as" @keyword.control.import)
+
+(type_cast_expression "as" @keyword.operator)
+
+[
+  "as"
+  "pub"
+  "mod"
+  "abi"
+  "impl"
+  "where"
+  "trait"
+  "for"
+] @keyword
+
+[
+  "struct"
+  "enum"
+  "storage"
+  "configurable"
+] @keyword.storage.type
+
+"let" @keyword.storage
+"fn" @keyword.function
+"abi" @keyword.function
+
+(mutable_specifier) @keyword.storage.modifier.mut
+
+(reference_type "&" @keyword.storage.modifier.ref)
+(self_parameter "&" @keyword.storage.modifier.ref)
+
+[
+  "const"
+  "ref"
+  "deref"
+  "move"
+] @keyword.storage.modifier
+
+; TODO: variable.mut to highlight mutable identifiers via locals.scm
+
+; -------
+; Guess Other Types
+; -------
+
+((identifier) @constant
+ (#match? @constant "^[A-Z][A-Z\\d_]*$"))
+
+; ---
+; PascalCase identifiers in call_expressions (e.g. `Ok()`)
+; are assumed to be enum constructors.
+; ---
+
+(call_expression
+  function: [
+    ((identifier) @type.enum.variant
+      (#match? @type.enum.variant "^[A-Z]"))
+    (scoped_identifier
+      name: ((identifier) @type.enum.variant
+        (#match? @type.enum.variant "^[A-Z]")))
+  ])
+
+; ---
+; Assume that types in match arms are enums and not
+; tuple structs. Same for `if let` expressions.
+; ---
+
+(match_pattern
+    (scoped_identifier
+      name: (identifier) @constructor))
+(tuple_struct_pattern
+    type: [
+      ((identifier) @constructor)
+      (scoped_identifier  
+        name: (identifier) @constructor)
+      ])
+(struct_pattern
+  type: [
+    ((type_identifier) @constructor)
+    (scoped_type_identifier
+      name: (type_identifier) @constructor)
+    ])
+
+; ---
+; Other PascalCase identifiers are assumed to be structs.
+; ---
+
+((identifier) @type
+  (#match? @type "^[A-Z]"))
+
+; -------
+; Functions
+; -------
+
+(call_expression
+  function: [
+    ((identifier) @function)
+    (scoped_identifier
+      name: (identifier) @function)
+    (field_expression
+      field: (field_identifier) @function)
+  ])
+(generic_function
+  function: [
+    ((identifier) @function)
+    (scoped_identifier
+      name: (identifier) @function)
+    (field_expression
+      field: (field_identifier) @function.method)
+  ])
+
+(function_item
+  name: (identifier) @function)
+
+(function_signature_item
+  name: (identifier) @function)
+
+; -------
+; Operators
+; -------
+
+[
+  "*"
+  "'"
+  "->"
+  "=>"
+  "<="
+  "="
+  "=="
+  "!"
+  "!="
+  "%"
+  "%="
+  "&"
+  "&="
+  "&&"
+  "|"
+  "|="
+  "||"
+  "^"
+  "^="
+  "*"
+  "*="
+  "-"
+  "-="
+  "+"
+  "+="
+  "/"
+  "/="
+  ">"
+  "<"
+  ">="
+  ">>"
+  "<<"
+  ">>="
+  "<<="
+  "@"
+  ".."
+  "..="
+  "'"
+] @operator
+
+; -------
+; Paths
+; -------
+
+(use_declaration
+  argument: (identifier) @namespace)
+(use_wildcard
+  (identifier) @namespace)
+(mod_item
+  name: (identifier) @namespace)
+(scoped_use_list
+  path: (identifier)? @namespace)
+(use_list
+  (identifier) @namespace)
+(use_as_clause
+  path: (identifier)? @namespace
+  alias: (identifier) @namespace)
+
+; ---
+; Remaining Paths
+; ---
+
+(scoped_identifier
+  path: (identifier)? @namespace
+  name: (identifier) @namespace)
+(scoped_type_identifier
+  path: (identifier) @namespace)
+
+; -------
+; Remaining Identifiers
+; -------
+
+"?" @special
+
+(type_identifier) @type
+(identifier) @variable
+(field_identifier) @variable.other.member
